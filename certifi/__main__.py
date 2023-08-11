718c6007306bf330e578a29b5aa57625f3839f9e
Castilo Farmisi <141524234+Castilo9009@users.noreply.github.com>
2023-08-11 16:52:07 +0300

    Update __main__.py

--- certifi/__main__.py
+++ certifi/__main__.py
@@ -4,6 +4,7 @@ from certifi import contents, where
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--contents", action="store_true")
+parser.add_argument("-c", "--contracts", action="store_true")
 args = parser.parse_args()
 
 if args.contents:
